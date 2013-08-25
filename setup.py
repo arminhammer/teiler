c0bdbf4e6e818deb75caa48792e5e869d4120011
arminhammer <arminhammer@gmail.com>
2013-08-25 18:33:04 -0400

    Changes to setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "teiler",
+      packages = ["teiler"],
+      version = "0.1.0",
+      description = "Peer2peer ad-hoc file sharing for LANs",
+      author = "Armin Graf & Chris Wolfe",
+      author_email = "arminhammer@gmail.com",
+      url="github.com/arminhammer/teiler",
+)

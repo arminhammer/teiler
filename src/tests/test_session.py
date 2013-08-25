5b3ce8a370c2b42c7c27e98a8f0e02205a00371c
arminhammer <arminhammer@gmail.com>
2013-08-25 01:59:14 -0400

    Added test_session and test method signatures

--- src/tests/test_session.py
+++ src/tests/test_session.py
@@ -1,46 +1,23 @@
-from binascii import crc32
-import os, json
-import Queue
-from twisted.protocols import basic
-from twisted.internet.protocol import ServerFactory
-from twisted.internet.protocol import ClientFactory
-from twisted.protocols.basic import FileSender, LineReceiver
-from twisted.internet.defer import Deferred
-from twisted.internet import reactor
-from twisted.python import log
-from filesender import FileSenderClientFactory
-import utils
-from message import Message
-from sessionmessageprotocol import SessionMessageFactory
+from twisted.trial import unittest
+from session import Session
 
-beginMsg = "BEGIN"
-acceptMsg = "ACCEPT"
-rejectMsg = "REJECT"
-receivedMsg = "RECEIVED"
-fileMsg = "FILE"
-dirMsg = "DIR"
-endFileMsg = "EOF"
-endMsg = "EOT"
-resendMsg = "RESEND"
-
-class Session(object):
+class SessionTestCase(unittest.TestCase):
     
-    def __init__(self, fileName, config, address, port):
-        self.id = utils.generateSessionID()
-        self.transferQueue = Queue.Queue()
-        self.address = address
-        self.port = port
-        self.fileName = fileName
-        self.status = 0
-        self.config = config
-       
-    def __str__(self):
+    def setUp(self):
+        pass
+    
+    def test_str(self):
+        ''' 
         return str(self.id)
+         '''
     
-    def startTransfer(self):
+    def test_startTransfer(self):
+        '''
         self.sendBeginning()
+        '''
          
-    def processResponse(self, msg):
+    def test_processResponse(self):
+        '''
         log.msg("Response received: {0}".format(msg))
         message = json.loads(msg)
         if message['command'] == acceptMsg and self.status == 1:
@@ -53,8 +30,10 @@ class Session(object):
             reactor.callLater(0, self.processTransferQueue)
         else:
             log.msg("NOT RECOGNIZED!")  
-    
-    def sendBeginning(self):
+        '''
+        
+    def test_sendBeginning(self):
+        '''
         beginMessage = Message(beginMsg, self.id)
         beginMessage.fileName = self.fileName
         log.msg("Sending BEGIN")
@@ -62,22 +41,28 @@ class Session(object):
         f = SessionMessageFactory(self, beginMessage)
         self.status = 1
         reactor.connectTCP(self.address, self.port, f)
+        '''
         
-    def sendEnd(self):
+    def test_sendEnd(self):
+        '''
         endMessage = Message(endMsg, self.id)
         log.msg("Sending EOT")
         f = SessionMessageFactory(endMessage)
         self.status = "finished"
         reactor.connectTCP(self.address, self.port, f)
         self.config.closeSession(self)
-    
-    def sendFile(self, path, address='localhost', port=1234):
+        '''
+        
+    def test_sendFile(self):
+        '''
         controller = type('test', (object,), {'cancel':False, 'total_sent':0, 'completed':Deferred()})
         f = FileSenderClientFactory(path, controller, self.id)
         reactor.connectTCP(address, port, f)
         return controller.completed
-    
-    def startFileSend(self):
+        '''
+        
+    def test_startFileSend(self):
+        '''
             log.msg("Calculating files...")
             self.transferQueue.put(self.fileName)
             if os.path.isdir(self.fileName):
@@ -89,8 +74,10 @@ class Session(object):
                         self.transferQueue.put(os.path.join(root, name))
                         log.msg("QUEUE: Adding file {0}".format(name))
                 reactor.callLater(0, self.processTransferQueue)
-       
-    def processTransferQueue(self):
+       '''
+        
+    def test_processTransferQueue(self):
+        '''
         remaining = self.transferQueue.qsize()
         log.msg("Processing queue.  Queue items remaining: {0}".format(remaining))
         if remaining == 0:
@@ -107,3 +94,5 @@ class Session(object):
                 reactor.connectTCP(self.address, self.port, f)
             else:
                 reactor.callLater(0, self.sendFile, path, self.address, self.port)
+        '''
+        

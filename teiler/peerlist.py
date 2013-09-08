39e2eb12c12370d495efe62df4bd655f56bef08f
arminhammer <arminhammer@gmail.com>
2013-09-07 21:05:19 -0400

    Formatting peerlist

--- teiler/peerlist.py
+++ teiler/peerlist.py
@@ -8,20 +8,11 @@ class PeerList(QWidget, AbstractPeerList):
     
     def __init__(self, parent=None):
         super(PeerList, self).__init__(parent)
-        # self.setVisible(True)
-        # self.setAcceptDrops(True)
-        # self.teiler.peerList.setDragEnabled(True)
-        # self.setViewMode(QListView.ListMode)
         self.layout = QVBoxLayout(self)
-        # self.setLayout(layout)
         self.setMinimumSize(240, 480)
-        # self.scrollArea = QScrollArea(self)
-        # self.scrollArea.setWidgetResizable(True)
-        self.peers = []
  
     def add(self, peer):
         self.layout.addWidget(peer)
-        # peer.show()
         self.connect(peer, SIGNAL("dropped"), self.notifyTeiler)
         log.msg("Peerlist: added peer " + str(peer))
         log.msg("Count is: " + str(self.layout.count()))
